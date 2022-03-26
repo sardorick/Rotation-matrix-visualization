1f7d6bc6f93b8afd016061215e07c093b6f27337
Sardorbek Zokirov <sardorzakirov1999@gmail.com>
2022-03-26 19:19:23 +0100

    Update Streamlit.py

--- Streamlit.py
+++ Streamlit.py
@@ -7,10 +7,9 @@ import plotly.express as px
 
 df = pd.read_csv("https://raw.githubusercontent.com/chriswmann/datasets/master/500_Person_Gender_Height_Weight_Index.csv")
 
-st.title("BMI Visualization")
-st.dataframe(df)
-data_select = st.sidebar.selectbox("Select your Scatter Plot", ("BMI", "BMI For males", "BMI For females"))
-hist_select = st.sidebar.selectbox("Choose which histogram you want", ("Male height", "Male weight", "Female height", "Female weight"))
+
+data_select = st.sidebar.selectbox("Select the data you want to see", ("Data overview", "BMI scatterplots", "Histogram data for wieghts in different categories"))
+# hist_select = st.sidebar.selectbox("Choose which histogram you want", ("Male height", "Male weight", "Female height", "Female weight"))
 
 
 rotation = st.sidebar.checkbox("Original Vector")
@@ -25,16 +24,16 @@ rotation = st.sidebar.checkbox("Original Vector")
 #     fig = px.scatter(xy, x='X axis', y='Y axis', title="Original Vector")
 #     st.plotly_chart(fig, use_column_width = True)
 
-rotation1 = st.sidebar.checkbox("Transformed Vector")
-deg = st.sidebar.slider("Choose your Rotation", 30, 100, 1)    
+# rotation1 = st.sidebar.checkbox("Transformed Vector")
+# deg = st.sidebar.slider("Choose your Rotation", 30, 100, 1)    
 
-def rot_mat(deg):
-    if rotation1 == "Transformed Vector":
-        st.title("Transformed Vector")
-        theta = deg/180*np.pi
-        cos = np.cos(theta)
-        sin = np.sin(theta)
-    return np.array([[cos, -sin], [sin, cos]])
+# def rot_mat(deg):
+#     if rotation1 == "Transformed Vector":
+#         st.title("Transformed Vector")
+#         theta = deg/180*np.pi
+#         cos = np.cos(theta)
+#         sin = np.sin(theta)
+#     return np.array([[cos, -sin], [sin, cos]])
 
 # r = rot_mat(deg)
 # rxy = r@xy
@@ -44,91 +43,81 @@ def rot_mat(deg):
     
 
 def load_data(data_select):
-# General scatter plot
-       if data_select == "BMI":
-           st.title("BMI")
-           fig = px.scatter( df,  x='Weight', y='Height', title='BMI', color='Index')
-                                
-
-           st.plotly_chart(fig, use_column_width = True)
-
-
-# Male BMI scatterplot
-
-       elif data_select == "BMI For males":
+        # General scatter plot
+        if data_select == "BMI scatterplots":
+            st.title("BMI")
+            fig = px.scatter( df,  x='Weight', y='Height', title='BMI', color='Index')
+            st.plotly_chart(fig, use_column_width = True)
+            # Male BMI scatterplot
             st.title("BMI For males")
             males = df.loc[(df['Gender'] == 'Male')]
             colorz = males.iloc[:, 3]
             fig = px.scatter(df.loc[(df['Gender'] == 'Male')],  x='Weight', y='Height', title='BMI For males', color=colorz)
             st.plotly_chart(fig, use_column_width = True)
 
-#Female BMI scatter plot
+            #Female BMI scatter plot
 
-       elif data_select == "BMI For Female":
             st.title("BMI For Female")
             females = df.loc[(df['Gender'] == 'Female')]
             colorz = females.iloc[:, 3]
             fig1 = px.scatter(df.loc[(df['Gender'] == 'Female')],  x='Weight', y='Height', title='BMI For females', color=colorz)
             st.plotly_chart(fig1, use_column_width = True)
+        
+        elif data_select == "Histogram data for wieghts in different categories":
+            # Males height
+            st.title('Histogram of males height')
+            for_hist  = df.loc[(df['Gender'] == 'Male')]
+            heightss = for_hist.iloc[:, 1]
+            df_heights = heightss.to_frame()
+            fig, ax = plt.subplots()
+            ax.hist(df_heights, bins=20)
+            plt.xlabel('Weight, kg')
+            plt.ylabel('Frequency')
+            st.pyplot(fig)
 
 
-       else:
-           print("no")
+            # Males weight
+            st.title('Histogram of males weight')
+            for_hist  = df.loc[(df['Gender'] == 'Male')]
+            weightss = for_hist.iloc[:, 2]
+            df_weights = weightss.to_frame()
+            fig, ax = plt.subplots()
+            ax.hist(df_weights, bins=20)
+            plt.xlabel('Weight, kg')
+            plt.ylabel('Frequency')
+            st.pyplot(fig)
+
+            # Female height
+            st.title('Histogram of female height')
+            for_hist  = df.loc[(df['Gender'] == 'Female')]
+            heightss = for_hist.iloc[:, 1]
+            df_heights = heightss.to_frame()
+            fig, ax = plt.subplots()
+            ax.hist(df_heights, bins=20)
+            plt.xlabel('Weight, kg')
+            plt.ylabel('Frequency')
+            st.pyplot(fig)
+
+        # Female weight
+            fig, ax = plt.subplots(figsize=(8,8))
+            st.title('Histogram of females weight')
+            for_hist  = df.loc[(df['Gender'] == 'Female')]
+            weightss = for_hist.iloc[:, 2]
+            df_weights = weightss.to_frame()
+            fig, ax = plt.subplots()
+            ax.hist(df_weights, bins=20)
+            plt.xlabel('Weight, kg')
+            plt.ylabel('Frequency')
+            st.pyplot(fig)
+        elif data_select == "Data overview":
+            st.title("BMI Visualization")
+            st.dataframe(df)
+        else:
+            print("no")
 
     
 load_data(data_select)
 
-def load_hist(hist_select):
-
-    # Males height
-    if hist_select == "Male height":
-        st.title('Histogram of males height')
-        for_hist  = df.loc[(df['Gender'] == 'Male')]
-        heightss = for_hist.iloc[:, 1]
-        df_heights = heightss.to_frame()
-        fig, ax = plt.subplots()
-        ax.hist(df_heights, bins=20)
-
-        st.pyplot(fig)
-
-
-    # Males weight
-    elif hist_select == "Male weight":
-        st.title('Histogram of males weight')
-        for_hist  = df.loc[(df['Gender'] == 'Male')]
-        weightss = for_hist.iloc[:, 2]
-        df_weights = weightss.to_frame()
-        fig, ax = plt.subplots()
-        ax.hist(df_weights, bins=20)
-
-        st.pyplot(fig)
-
-    # Female height
-    elif hist_select == "Female height":
-        st.title('Histogram of female height')
-        for_hist  = df.loc[(df['Gender'] == 'Female')]
-        heightss = for_hist.iloc[:, 1]
-        df_heights = heightss.to_frame()
-        fig, ax = plt.subplots()
-        ax.hist(df_heights, bins=20)
-
-        st.pyplot(fig)
-
-    # Female weight
-    elif hist_select == "Female weight":
-        fig, ax = plt.subplots(figsize=(8,8))
-        st.title('Histogram of females weight')
-        for_hist  = df.loc[(df['Gender'] == 'Female')]
-        weightss = for_hist.iloc[:, 2]
-        df_weights = weightss.to_frame()
-        fig, ax = plt.subplots()
-        ax.hist(df_weights, bins=20)
-        st.pyplot(fig)
-
-    else:
-        print("no")
-
-load_hist(hist_select)
 
 
 # Plot mean of Weight and heigh
